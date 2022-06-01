757ad3e626b375cc543f3b3aa5702dfa2d0191be
Kevin Ly <lykt2@vcu.edu>
2022-06-01 14:04:27 -0400

    Commented on the Terms and Conditions Page

--- src/screens/TermsConditions.js
+++ src/screens/TermsConditions.js
@@ -3,6 +3,7 @@ import {
 } from 'react-native';
 import React from 'react';
 
+// This page is for the Terms & Conditions page with the Privacy Policy
 const TermsConditions = () => {
     return (
         <Text>
