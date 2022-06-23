5863d9c18671a67a9a3fc86101d12a73a172a40f
Kevin Ly <kevinly277@gmail.com>
2022-06-23 10:21:56 -0400

    imported stylesheets and added the categoriescard component

--- src/screens/FinancialLiteracyCategories.js
+++ src/screens/FinancialLiteracyCategories.js
@@ -6,10 +6,13 @@ import {
 const FinancialLiteracyCategories = () =>
 {
 
+    
 }
 
 const styles = StyleSheet.create({
-    
+    container: {
+
+    },
 })
 
 export default FinancialLiteracyCategories;
