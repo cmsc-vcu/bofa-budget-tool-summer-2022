53fbecedbbdefab763e35a2c1f23380c0e3bfef3
Kevin Ly <lykt2@vcu.edu>
2022-06-23 12:08:18 -0400

    Added a flow to the modules, added navigation on the App.js and added orange button on the categories screen

---
+++ src/screens/FinancialLiteracyModules.js
@@ -0,0 +1,25 @@
+import {
+    StyleSheet,
+    Text,
+} from 'react-native';
+import React from 'react';
+
+
+const FinancialLiteracyModules = () =>
+{
+    return (
+        <Text>
+            Modules
+        </Text>
+        
+    );
+        
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+})
+
+export default FinancialLiteracyModules;
