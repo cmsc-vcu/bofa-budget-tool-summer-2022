82b4e2499d8c8a7d989a1a106c19101d06096490
Kevin Ly <lykt2@vcu.edu>
2022-06-23 11:39:48 -0400

    added activebookmark and made financial tips for dynamic with the useState

--- src/screens/FinancialLiteracyCategories.js
+++ src/screens/FinancialLiteracyCategories.js
@@ -1,12 +1,17 @@
 import {
-    StyleSheet
-} from 'react-native'
+    StyleSheet,
+    Text
+} from 'react-native';
+import React from 'react';
+import OrangeButton from '../components/OrangeButton';
 
 
 const FinancialLiteracyCategories = () =>
 {
-
-    
+    return (
+        <Text> Hello World from FinancialLiteracyCategories! </Text>
+    );
+        
 }
 
 const styles = StyleSheet.create({
