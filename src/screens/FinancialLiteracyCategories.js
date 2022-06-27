385e3e0b5827a3e25a231fe09e818db226180571
Kevin Ly <lykt2@vcu.edu>
2022-06-27 11:45:11 -0400

    added the default image and fixed formatting in the categories page

--- src/screens/FinancialLiteracyCategories.js
+++ src/screens/FinancialLiteracyCategories.js
@@ -49,6 +49,39 @@ const FinancialLiteracyCategories = () =>
             </View>
             
             <ScrollView style={styles.container} contentContainerStyle={styles.innerScrollViewContainer}>
+
+
+                {/* PERSONA CARD */}
+                <View style={styles.personaCard}>
+                    <View style={styles.personaImageContainer}>
+                        <Image source={require('../res/images/defaultPersonaImage.png')} style={styles.personaImage}>
+
+                        </Image>
+                    </View>
+                    
+                    <View style={styles.personaTextContainer}>
+                        <Text style={styles.personaName}>
+                            {/* Persona Name goes Here */}
+                            Emilio Johnson
+                        </Text>
+                        <Text style={styles.personaJob}>
+                            {/* Persona Job Title goes Here */}
+                            Graphic Designer
+                        </Text>
+                        <Text style={styles.personaResidence}>
+                            {/* Persona Residence goes Here */}
+                            Richmond, VA
+                        </Text>
+                    </View>
+                </View>
+
+                {/* Career Snapshot */}
+                <View style={styles.careerSnapshotContainer}>
+                    <Text>View Career Snapshot</Text>
+                </View>
+
+                
+
                 <View>
                     <OrangeButton
                         navigatepage='FinancialLiteracyModules'
@@ -66,8 +99,10 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     innerScrollViewContainer: {
+        flex: 1,
         flexGrow: 1,
-        justifyContent: 'center'
+        alignItems: 'center',
+        backgroundColor: '#CCDBBF',
     },
     settings: {
         textAlign: 'center',
@@ -115,6 +150,70 @@ const styles = StyleSheet.create({
     topnavtabcontainer: {
         width: '100%',
         height: '14%'
+    },
+    personaCard: {
+        width: '90%',
+        height: '22%',
+        borderRadius: 22,
+        backgroundColor: figmaColors.primaryOffWhite,
+        marginTop: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        shadowColor: '#000000',
+        shadowOffset: {
+            width: 0,
+            height: 4
+        },
+        shadowRadius: 4,
+        shadowOpacity: 1,
+        elevation: 4,
+    },
+    personaImageContainer: {
+        // Do not add width
+        // It'll make the container contain % of the personaCard, which will make it
+        // look like there's padding between the image and text
+        width: '30%',
+        height: '70%',
+        resizeMode: 'repeat',
+        marginLeft: 14
+    },
+    personaTextContainer: {
+        marginRight: 100
+    },
+    personaName: {
+        fontStyle: 'normal',
+        fontWeight: '700',
+        fontSize: 28,
+        lineHeight: 33,
+        color: '#000000'
+    },
+    personaJob: {
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: 20,
+        lineHeight: 23,
+    },
+    personaResidence: {
+        fontStyle: 'normal',
+        fontWeight: '300',
+        fontSize: 15,
+    },
+    careerSnapshotContainer: {
+        width: '90%',
+        height: '12%',
+        borderRadius: 22,
+        backgroundColor: figmaColors.primaryOffWhite,
+        marginTop: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        shadowColor: '#000000',
+        shadowOffset: {
+            width: 0,
+            height: 4
+        },
+        shadowRadius: 4,
+        shadowOpacity: 1,
+        elevation: 4,
     }
 })
 
