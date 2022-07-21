66315a373ac8f24f279d0b7a106a986697f645a0
Kevin Ly <lykt2@vcu.edu>
2022-07-21 15:44:00 -0400

    New Card Intro v2

--- src/screens/CardIntro.js
+++ src/screens/CardIntro.js
@@ -8,6 +8,7 @@ import {
     StyleSheet,
     View,
     Image,
+    ImageBackground,
     Dimensions
 } from 'react-native'
 import figmaColors from "../res/figmaColors";
@@ -16,6 +17,7 @@ import OrangeButton from "../components/OrangeButton";
 import { useNavigation } from "@react-navigation/native";
 import Carousel from 'react-native-snap-carousel';
 import { Pagination } from "react-native-snap-carousel";
+import CircularBackground from '../res/svg/cardIntroCircularBackground.svg'
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
@@ -26,19 +28,19 @@ const height = Dimensions.get('window').height;
 
 const cardItems = [
     {
-        ImagePath: require('../res/images/financialtarget.png'),
-        Title: 'Define your\nfinancial\ngoals.',
-        subtitle: 'Don\'t know what\'s your need?\nDon\'t worry, we will guide you\nstep by step.'
+        ImagePath: require('../res/images/cardIntroFirstImage.png'),
+        Title: 'Budgeting\nfor your\nstudent\nsuccess.',
+        subtitle: 'We know financial literacy is\nnot always the top of your\npriority when you have classes\nand a life to juggle.'
     },
     {
-        ImagePath: require('../res/images/Yourpath.png'),
-        Title: 'Lead your\nown path.',
-        subtitle: 'Build a financial path that best\nsuits you.'
+        ImagePath: require('../res/images/cardIntroSecondImage.png'),
+        Title: 'Conquer\nyour\nfinancial\ngoals.',
+        subtitle: 'So we\'re here to help you\nnavigate your finance to achieve\nyour goals--whether it\'s to buy a\nnew PC or to pay off your debt.'
     },
     {
-        ImagePath: require('../res/images/Conquerfuture.png'),
-        Title: 'Conquer your\nfuture.',
-        subtitle: 'Prepare with strong financial\ncompetency for your life after college.'
+        ImagePath: require('../res/images/cardIntroThirdImage.png'),
+        Title: 'Get started\non your\njourney to \nfinancial\nfreedom.',
+        subtitle: 'This is your first step to\naffording the lifestyle you want.'
     }
 ]
 
@@ -67,20 +69,23 @@ function CardIntro (props) {
     // This acts as a template for the cards
     const renderCardItem = useCallback(({item, index}) => (
         <View style={styles.subcontainer}>
-            <Image source={ item.ImagePath } />
-            <Text style={[styles.title, { paddingTop: 0, paddingBottom: 20 }]}>{item.Title}</Text>
-            <Text style={styles.subtitle}>{item.subtitle}</Text>
+            <Image style={styles.imageSize} source={ item.ImagePath }/>
+            <View>
+                <Text style={[styles.title, { paddingTop: 0, paddingBottom: 20 }]}>{item.Title}</Text>
+                <Text style={styles.subtitle}>{item.subtitle}</Text>
+            </View>
         </View>
     ), []);
 
     return (
         <View style={styles.container}>
+            <CircularBackground style={styles.circularBackgroundStyle}/>
                 <Carousel
                     layout="default"
                     ref={ref}
                     data={carouselItems}
-                    sliderWidth={width * 0.98}
-                    itemWidth={width * 0.9}
+                    sliderWidth={width * 1}
+                    itemWidth={width * 1}
                     renderItem={renderCardItem}
                     onSnapToItem={(index) => setActiveIndex(index)}
                     paddingRight={100}
@@ -105,28 +110,14 @@ const styles = StyleSheet.create({
     // This is the main container that controls the WHOLE screen
     container: {
         flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
         backgroundColor: figmaColors.primaryOffWhite,
-        paddingBottom: 50,
-        paddingTop: 20
-        
+        paddingTop: 40,
+        paddingBottom: 30,
     },
     // Be care of adding left and bottom attributes when pulling from figma
     // This acts as the card container
     subcontainer: {
-        alignItems: 'flex-start',
-        borderRadius: 30,
-        backgroundColor: figmaColors.primaryGreen,
-        width: width * 0.9,
-        height: height * 0.9,
-        paddingLeft: 35,
-        paddingTop: 15,
-        shadowColor: '#000000',
-        shadowOffset: {width: 0, height: 4},
-        shadowRadius: 4,
-        shadowOpacity: 1,
-        elevation: 5,
+        alignItems: 'center',
     },
     // This refers to the main title of the cards
     title: {
@@ -135,8 +126,6 @@ const styles = StyleSheet.create({
         fontSize: 40,
         fontWeight: '800',
         color: figmaColors.primaryOffWhite,
-        paddingTop: 50,
-        paddingRight: 30
     },
     // This refers to the subtitle of the card
     subtitle: {
@@ -145,22 +134,29 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: '400',
         color: figmaColors.primaryOffWhite,
-        lineHeight: 28,
-        paddingBottom: 35
+        lineHeight: 27,
     },
     activeDotStyle: {
         width:20,
         height:20,
         borderRadius: 10,
         marginHorizontal: 8,
-        backgroundColor: figmaColors.primaryGreen
+        backgroundColor: figmaColors.primaryOffWhite
     },
     inactiveDotStyle: {
-        borderColor: figmaColors.primaryGreen,
-        borderWidth: 2,
-        backgroundColor: figmaColors.primaryOffWhite
+        borderColor: figmaColors.primaryOffWhite,
+        borderWidth: 1,
+        backgroundColor: 'transparent'
+    },
+    circularBackgroundStyle: {
+        position: 'absolute',
+        top: width * 0.4
+    },
+    imageSize: {
+        width: width * 0.7,
+        height: height * 0.3,
+        resizeMode: 'contain'
     }
-
 });
 
 export default CardIntro;
